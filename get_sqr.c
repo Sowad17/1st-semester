8005e143ea8420f65aaee36a60b532d128f73e7b
Sowad17 <38504185+Sowad17@users.noreply.github.com>
2018-04-23 15:45:53 +0600

    Add files via upload

---
+++ get_sqr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int get_sqr(void);
+int main(void)
+{
+int sqr;
+sqr = get_sqr();
+printf("Square : %d ",sqr);
+return 0;
+}
+int get_sqr(void)
+{
+    int num ;
+    printf("Enter The Number :");
+    scanf("%d",&num);
+    return num*num;
+}
